2bc2d7e8a2bf1aefed9b7eb99c3bad2fe3be4c18
SofranSebastian <sofransebastian@yahoo.com>
2025-02-12 17:05:08 +0200

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <meta name="apple-mobile-web-app-title" content="MOCHETINO - Spalatorie covoare Timișoara" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
